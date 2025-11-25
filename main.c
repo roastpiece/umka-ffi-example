47fbaf74b02d0e65c537795e16b1f8ef79e13949
Michael Noser <michael@noser.moe>
2025-11-25 12:30:35 +0100

    Add floats

--- main.c
+++ main.c
@@ -9,6 +9,25 @@ void bar(int num, int num2) {
     printf("Yo 2 numbers lmao: %d,%d\n", num, num2);
 }
 
+void doubleargs(double num, double num2) {
+    printf("I have double args: %f,%f\n", num, num2);
+}
+
+void floatargs(float num, float num2) {
+    printf("I have float args: %f,%f\n", num, num2);
+}
+
+void mixed(double num, int num2, double num3, int num4) {
+    printf("I have mixed args: %f, %d, %f, %d\n", num, num2, num3, num4);
+}
+
+void intsizes(int8_t num, int16_t num2, int32_t num3, int64_t num4) {
+    printf("I have ints args: %d, %d, %d, %ld\n", num, num2, num3, num4);
+}
+void booltype(bool b) {
+    printf("I have bool: %b", b);
+}
+
 int main(int argc, char **argv) {
     Umka *umka = umkaAlloc();
 
